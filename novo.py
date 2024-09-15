ba779a9cb2d86379b6375288b9b2a1f4b6a6dad7
jaugustodantas <jnetodf@gmail.com>
2024-09-15 18:20:20 -0300

    primeiro commit

---
+++ novo.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template,redirect,request,session,url_for,make_response
+import webbrowser
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import csv
+from io import StringIO
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db= SQLAlchemy(app)
+
+
+from views import *    
+
+
+if __name__ == '__main__':
+    webbrowser.open('http://127.0.0.1:5000/login')
+    app.run(debug=True)
+   # app.run()
+
+
+
